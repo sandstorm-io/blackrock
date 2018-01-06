866bcd140f560f1453166973bf01ea15e8af5a74
Kenton Varda <kenton@sandstorm.io>
2018-01-06 14:30:48 -0800

    gcloud create changed behavior

--- release.sh
+++ release.sh
@@ -119,9 +119,9 @@ mkdir -p dbg
 cp bin/blackrock.unstripped dbg/blackrock-$BUILDSTAMP
 
 # Create a new image.
-doit gce instances create build --image debian-9
+doit gce instances create build --image-project debian-cloud --image-family debian-9
 doit sleep 10 # make sure instance is up
-doit gce ssh build 'sudo sed -i -e "s/PermitRootLogin no/PermitRootLogin without-password/g" /etc/ssh/sshd_config; sudo service ssh restart'
+doit gce ssh build --command 'sudo sed -i -e "s/PermitRootLogin no/PermitRootLogin without-password/g" /etc/ssh/sshd_config; sudo service ssh restart'
 doit gce copy-files blackrock.tar.xz root@build:/
 doit gce ssh root@build --command "cd / && tar Jxof blackrock.tar.xz && rm /blackrock.tar.xz"
 doit gce instances delete build -q --keep-disks boot
